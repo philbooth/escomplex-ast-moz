9c1bd9ad95a3838752bc2b414a4a57425a1ffb3d
Phil Booth <pmbooth@gmail.com>
2013-10-19 22:04:27 +0100

    Change interface.

--- src/index.js
+++ src/index.js
@@ -2,65 +2,67 @@
 
 'use strict';
 
-var check, syntaxDefinitions, syntaxes, callbacks;
+var check, syntaxDefinitions;
 
 check = require('check-types');
 syntaxDefinitions = require('./syntax');
 
 exports.walk = walk;
 
-function initialise (settings, callbacksArg) {
-    syntaxes = syntaxDefinitions.get(settings);
+function walk (tree, settings, callbacks) {
+    var syntaxes
+
     check.verifyObject(callbacks, 'Invalid callbacks');
     check.verifyFunction(callbacks.processNode, 'Invalid processNode callback');
     check.verifyFunction(callbacks.createScope, 'Invalid createScope callback');
-    callbacks = callbacksArg;
-}
 
-function walk (tree, assignedName) {
-    check.isObject(syntaxes, 'Uninitialised');
-    check.isObject(callbacks, 'Uninitialised');
-    check.verifyArray(tree, 'Invalid syntax tree');
+    syntaxes = syntaxDefinitions.get(settings);
 
-    tree.forEach(function (node) {
-        visitNode(node, assignedName);
-    });
-}
+    visitNodes(tree);
 
-function visitNode (node, assignedName) {
-    var syntax;
+    function visitNodes (nodes, assignedName) {
+        check.verifyArray(tree, 'Invalid syntax tree');
 
-    if (check.isObject(node)) {
-        syntax = syntaxes[node.type];
-
-        if (check.isObject(syntax)) {
-            callbacks.processNode(node, syntax);
-
-            if (syntax.newScope) {
-                callbacks.createScope(node.id, assignedName, node.loc, node.params.length);
-            }
-
-            visitChildren(node);
-        }
-    }
-}
-
-function visitChildren (node) {
-    var syntax = syntaxes[node.type];
-
-    if (check.isArray(syntax.children)) {
-        syntax.children.forEach(function (child) {
-            visitChild(
-                node[child],
-                check.isFunction(syntax.assignableName) ? syntax.assignableName(node) : '',
-                currentReport
-            );
+        tree.forEach(function (node) {
+            visitNode(node, assignedName);
         });
     }
-}
 
-function visitChild (child, assignedName) {
-    var visitor = check.isArray(child) ? walk : visitNode;
-    visitor(child, assignedName);
+    function visitNode (node, assignedName) {
+        var syntax;
+
+        if (check.isObject(node)) {
+            syntax = syntaxes[node.type];
+
+            if (check.isObject(syntax)) {
+                callbacks.processNode(node, syntax);
+
+                if (syntax.newScope) {
+                    callbacks.createScope(node.id, assignedName, node.loc, node.params.length);
+                }
+
+                visitChildren(node);
+            }
+        }
+    }
+
+    function visitChildren (node) {
+        var syntax = syntaxes[node.type];
+
+        if (check.isArray(syntax.children)) {
+            syntax.children.forEach(function (child) {
+                visitChild(
+                    node[child],
+                    check.isFunction(syntax.assignableName) ? syntax.assignableName(node) : '',
+                    currentReport
+                );
+            });
+        }
+    }
+
+    function visitChild (child, assignedName) {
+        var visitor = check.isArray(child) ? visitNodes : visitNode;
+        visitor(child, assignedName);
+    }
 }
 
